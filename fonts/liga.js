62ec1fa14ac73d0e32518125aa57b828c806a35b
Smorodin Kirill <smokir@ya.ru>
2015-09-08 00:23:26 +0300

    remake *dirty

---
+++ fonts/liga.js
@@ -0,0 +1,77 @@
+/* A polyfill for browsers that don't support ligatures. */
+/* The script tag referring to this file must be placed before the ending body tag. */
+
+/* To provide support for elements dynamically added, this script adds
+   method 'icomoonLiga' to the window object. You can pass element references to this method.
+*/
+(function () {
+	'use strict';
+	function supportsProperty(p) {
+		var prefixes = ['Webkit', 'Moz', 'O', 'ms'],
+			i,
+			div = document.createElement('div'),
+			ret = p in div.style;
+		if (!ret) {
+			p = p.charAt(0).toUpperCase() + p.substr(1);
+			for (i = 0; i < prefixes.length; i += 1) {
+				ret = prefixes[i] + p in div.style;
+				if (ret) {
+					break;
+				}
+			}
+		}
+		return ret;
+	}
+	var icons;
+	if (!supportsProperty('fontFeatureSettings')) {
+		icons = {
+			'spinner': '&#xe60f;',
+			'sliders': '&#xe601;',
+			'play': '&#xe602;',
+			'pause': '&#xe603;',
+			'stop': '&#xe604;',
+			'backward': '&#xe605;',
+			'forward': '&#xe606;',
+			'volume-high': '&#xe607;',
+			'volume-medium': '&#xe608;',
+			'volume-low': '&#xe609;',
+			'volume-mute': '&#xe60b;',
+			'shuffle': '&#xe60c;',
+			'list-add': '&#xe01e;',
+			'repeat': '&#xe023;',
+			'repeat-one': '&#xe024;',
+			'0': 0
+		};
+		delete icons['0'];
+		window.icomoonLiga = function (els) {
+			var classes,
+				el,
+				i,
+				innerHTML,
+				key;
+			els = els || document.getElementsByTagName('*');
+			if (!els.length) {
+				els = [els];
+			}
+			for (i = 0; ; i += 1) {
+				el = els[i];
+				if (!el) {
+					break;
+				}
+				classes = el.className;
+				if (/icon_/.test(classes)) {
+					innerHTML = el.innerHTML;
+					if (innerHTML && innerHTML.length > 1) {
+						for (key in icons) {
+							if (icons.hasOwnProperty(key)) {
+								innerHTML = innerHTML.replace(new RegExp(key, 'g'), icons[key]);
+							}
+						}
+						el.innerHTML = innerHTML;
+					}
+				}
+			}
+		};
+		window.icomoonLiga();
+	}
+}());
--- scripts/app.js
+++ scripts/app.js
@@ -3,109 +3,96 @@
 
     app.Player = function () {
         var self = this;
-
-        self.context = app.AudioContext;
-        self.gainNode = self.context.createGain();
+        
+        self.initDragDrop();
+        
+        self.tracks = ko.observableArray();;
         self.currentTrack = ko.observable();
-        self.isPlaying = ko.observable(false);
-
-        self.analyser = self.context.createAnalyser();
-        self.analyser.fftSize = 256;
-        self.canvasCtx = (document.getElementsByClassName("player__canvas")[0]).getContext("2d");
-
-        self.initDragAndDrop();
     };
 
-    app.Player.prototype.open = function (file) {
-        var self = this,
-            reader = new FileReader(),
-            context = app.AudioContext,
-            tags,
-            cover, base64String, coverUrl,
-            track;
+    app.Player.prototype.open = function (files) {
+        var self = this;
+        
+        function open(file) {
+            var reader = new FileReader(),
+                audioContext = app.AudioContext,
+                tags,
+                picture,
+                base64String = "",
+                pictureUrl,
+                track;
+            
+            ID3.loadTags(file, function () {
+                tags = ID3.getAllTags(file);
 
-        ID3.loadTags(file, function () {
-            tags = ID3.getAllTags(file);
+                if (tags.picture) {
+                    picture = tags.picture;
+                    for (var i = 0; i < picture.data.length; i++) {
+                        base64String += String.fromCharCode(picture.data[i]);
+                    }
 
-            if (tags.picture) {
-                cover = tags.picture;
-                for (var i = 0; i < cover.data.length; i++) {
-                    base64String += String.fromCharCode(cover.data[i]);
+                    pictureUrl = "data:" + picture.format + ";base64," + window.btoa(base64String);
+                }
+                else {
+                    pictureUrl = "images/defaultPicture.png"
                 }
 
-                coverUrl = "data:" + cover.format + ";base64," + window.btoa(base64String);
-            }
-            else {
-                coverUrl = "images/unknown.png"
-            }
+                track = {
+                    loaded: ko.observable(false),
+                    name: file.name,
+                    artist: tags.artist || "unknown",
+                    title: tags.title || "unknown",
+                    album: tags.album || "unknown",
+                    year: tags.year,
+                    picture: pictureUrl,
+                }
 
-            track = {
-                loaded: ko.observable(false),
-                name: file.name,
-                artist: tags.artist || "unknown",
-                title: tags.title || "unknown",
-                cover: coverUrl,
-            }
+                reader.onload = function () {
+                    audioContext.decodeAudioData(reader.result, function (audio) {
+                        track.loaded(true);
+                        track.audio = audio;
+                    });
 
-            reader.onload = function () {
-                context.decodeAudioData(reader.result, function (audio) {
-                    track.loaded(true);
-                    track.audio = audio;
+                    self.tracks.push(track);
+                };
+
+                reader.readAsArrayBuffer(file);
+            },
+                {
+                    tags: ["artist", "title", "album", "year", "picture"],
+                    dataReader: FileAPIReader(file)
                 });
-
-                self.currentTrack(track);
-            };
-
-            reader.readAsArrayBuffer(file);
-        },
-            {
-                tags: ["artist", "title", "cover"],
-                dataReader: FileAPIReader(file)
-            });
-    };
-
-    app.Player.prototype.play = function () {
-        var self = this;
-
-        if (self.currentTrack().loaded()) {
-            self.source = self.context.createBufferSource();
-            self.source.buffer = self.currentTrack().audio;
-            self.source.connect(self.analyser);
-            self.source.connect(self.gainNode);
-            self.gainNode.connect(self.context.destination);
-            self.source.start(0);
-            self.isPlaying(true);
-            self.visualize();
         }
-    }
 
-    app.Player.prototype.stop = function () {
+        for (var i = 0; i < files.length; i++) {
+            open(files[i]);
+        }
+    };
+    
+    app.Player.prototype.initDragDrop = function () {
         var self = this;
+        
+        var jsDrop = document.getElementsByClassName("js-drop")[0];
+        
+        jsDrop.addEventListener("dragenter", function (e) {
+            this.classList.add("player__drop_overlay");
+        });
 
-        self.source.stop();
-        self.isPlaying(false);
-    }
+        jsDrop.addEventListener('dragleave', function (e) {
+            this.classList.remove("player__drop_overlay");
+        });
 
-    app.Player.prototype.initDragAndDrop = function () {
-        var self = this;
-
-        document.getElementsByClassName("player__drag")[0].addEventListener('drop', function (e) {
-            var files = e.dataTransfer.files;
-
+        jsDrop.addEventListener('drop', function (e) {
             if (e.preventDefault) {
                 e.preventDefault();
             }
 
-            for (var key in files) {
-                if (typeof (files[key]) == 'object' && files[key].type.indexOf('audio') > -1) {
-                    self.open(files[key]);
-                }
-            };
+            this.classList.remove("player__drop_overlay");
 
-            return false;
+            self.open(e.dataTransfer.files);
         });
 
-        document.getElementsByClassName("player__drag")[0].addEventListener('dragover', function (e) {
+        jsDrop.addEventListener('dragover', function (e) {
             if (e.preventDefault) {
                 e.preventDefault();
             }
@@ -114,46 +101,6 @@
 
             return false;
         });
-    }
-
-    app.Player.prototype.visualize = function () {
-        var self = this,
-            bufferLength = self.analyser.frequencyBinCount,
-            dataArray = new Uint8Array(bufferLength),
-            WIDTH = self.canvasCtx.canvas.width,
-            HEIGHT = self.canvasCtx.canvas.height;
-
-        var draw = function () {
-            var barWidth = (WIDTH / bufferLength) * 2.5,
-                barHeight,
-                drawVisual,
-                x = 0;
-
-            if (self.isPlaying()) {
-                drawVisual = requestAnimationFrame(draw);
-
-                self.analyser.getByteFrequencyData(dataArray);
-                self.canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-                for (var i = 0; i < bufferLength; i++) {
-                    barHeight = dataArray[i] / 2;
-
-                    self.canvasCtx.fillStyle = 'rgb(255,0,0)';
-                    self.canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
-
-                    x += barWidth + 1;
-                }
-            }
-            else {
-                for (var i = 0; i < bufferLength; i++) {
-                    self.canvasCtx.fillStyle = 'rgb(255,0,0)';
-                    self.canvasCtx.fillRect(x, 0, barWidth, 0);
-
-                    x += barWidth + 1;
-                }
-            }
-        };
-
-        draw();
-    }
+    };
 
 } (window.app = window.app || {}));
